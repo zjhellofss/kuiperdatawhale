9fbee832c64b5783fb348b162596739479037ffe
fushenshen <hellofss@foxmail.com>
2023-06-02 21:47:09 +0800

    更新课件

--- course1/test/axby.cpp
+++ course1/test/axby.cpp
@@ -45,6 +45,7 @@ TEST(test_arma, e_power_minus) {
   fmat y;
   EPowerMinus(x, y);
   std::vector<float> x1(x.mem, x.mem + 224 * 224);
+  ASSERT_EQ(y.empty(), false);
   for (int i = 0; i < 224 * 224; ++i) {
     ASSERT_LE(std::abs(std::exp(-x1.at(i)) - y.at(i)), 1e-5f);
   }
@@ -56,7 +57,6 @@ void Axpy(const arma::fmat &x, arma::fmat &Y, float a, float y) {
 
 TEST(test_arma, axpy) {
   using namespace arma;
-
   fmat x(224, 224);
   x.randu();
 
@@ -65,6 +65,7 @@ TEST(test_arma, axpy) {
   float y = 4.f;
   Axpy(x, Y, a, y);
 
+  ASSERT_EQ(Y.empty(), false);
   std::vector<float> x1(x.mem, x.mem + 224 * 224);
   for (int i = 0; i < 224 * 224; ++i) {
     ASSERT_LE(std::abs(x.at(i) * a + y - Y.at(i)), 1e-5f);
